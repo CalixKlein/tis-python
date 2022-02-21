dee8c40dc799c536c6f8439a9723cc68f65a0b00
Sindongin <corean1225@naver.com>
2022-02-21 15:21:50 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,103 @@
+#hello.py
+"""
+print(1+1)
+
+
+print("Hello world")
+"""
+"""
+print(1+1)
+print("Hello world")
+"""
+
+"""
+a = 123
+print(a)
+
+a = 2.3
+print(a)
+
+a = 3
+b = 4
+
+print(a + b)
+
+print (a/b) #소수점
+print( 7//3) #몫을 구하는 연산자
+print(2 ** 3) #2^3
+print(7%3) #나머지를 구하는 연산자
+"""
+
+
+"Hello world" #둘다 문자열
+'Hello world' 
+
+p1 = "python's good"
+p2 = 'python"s good'
+
+
+print(p1)
+print(p2)
+
+
+content = "Life is too shorte you need python"
+print(content)
+content2 = """
+Life is too short
+you need python
+"""
+#print(content2)
+
+#head = "python"
+#tail = " is fun"
+
+#print(head + tail)
+
+#print(head + 2) #error 
+
+#a = "pyhon"
+
+#print(a * 2)
+
+#print("=" * 30)
+#print("Hello world")
+#print("=" * 30)
+
+#문자열 인덱싱과 슬라이싱
+a = "Life is too short, You need Python"
+    #1234
+
+#print(a[3])
+
+#print(a[33])
+#print(a[-1])
+
+#b = a[0] + a[1] + a[2] + a[3]
+#print(b)
+#print(a[0:4])
+#print(a[12:17])
+#print(a[:17])
+#print(a[19:])
+#print(a[:])
+#print(a[19:-7])
+
+#a = "20010331Rainy"
+#data = a[:8]
+#weather = a[8:]
+
+#print(data)
+#print(weather)
+
+#"pithon" -> "python"
+
+#a = "pithon"
+# a[1] = "y" error
+
+#a = a[0] + "y" + a[2:]
+#print(a)
+
+#문자열 포매팅
+
+a = "I eat %d apples." %3
+
+print(a)
